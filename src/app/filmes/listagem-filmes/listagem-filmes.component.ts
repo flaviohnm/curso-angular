3eeadece670eccec8c700194bbb7687ca65c4e24
Flavio Monteiro <flaviohnm@gmail.com>
2021-05-02 14:27:50 -0300

    NG template e melhoria de performance na busca

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,9 @@
-import { ConfigPrams } from './../../shared/models/config-prams';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { debounceTime } from 'rxjs/operators';
+
+import { ConfigPrams } from './../../shared/models/config-prams';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -10,6 +13,8 @@ import { Filme } from 'src/app/shared/models/filme';
   styleUrls: ['./listagem-filmes.component.scss']
 })
 export class ListagemFilmesComponent implements OnInit {
+
+  readonly semFoto = '/src/assets/images/semFoto.jpeg';
 
   config: ConfigPrams = {
     pagina: 0,
@@ -29,7 +34,9 @@ export class ListagemFilmesComponent implements OnInit {
 
     });
 
-    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+    this.filtrosListagem.get('texto').valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((val: string) => {
         this.config.pesquisa = val;
         this.resetarConsulta();
     });
@@ -61,3 +68,4 @@ export class ListagemFilmesComponent implements OnInit {
   }
 
 }
+
